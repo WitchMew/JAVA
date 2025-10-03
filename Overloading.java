2b0e8777a292fa51f3d9083a484c817d20db366c
Asmita Singh <singhasmita290704@gmail.com>
2025-10-03 10:33:00 +0530

    Overloading

---
+++ Overloading.java
@@ -0,0 +1,14 @@
+public class Overloading {
+void sum(){
+    System.out.println("Hello I'm Sum method");
+}
+void sum(int a,int b){
+    System.out.println("The sum is :"+(a+b));
+}
+public static void main(String[]args)
+{
+    Overloading obj=new Overloading();
+    obj.sum();
+    obj.sum(20,54);
+}
+}
